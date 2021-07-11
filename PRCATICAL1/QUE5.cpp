6b423cfac540acb921fefdf57afe284bd78ab9f7
SHIVANI_88005 <75790276+SHIVANI88005@users.noreply.github.com>
2021-07-11 18:24:39 +0530

    Create QUE5.cpp

---
+++ PRCATICAL1/QUE5.cpp
@@ -0,0 +1,17 @@
+// QUES5. Given a full m-ary tree with i internal vertices, Write a Program to find the number of leaf nodes.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m,l,i;
+
+    cout<<"enter the degree of tree:";
+    cin>>m;
+
+    cout<<"enter the value of internal vertices:";
+    cin>>i;
+
+    l=i*(m-1)+1;
+
+    cout<<"the number of leaves:"<<l<<endl;
+}
